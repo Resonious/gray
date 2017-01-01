76f18f9a369f78977bd9887c81402e97cfbe6cd4
Nigel Baillie <metreckk@gmail.com>
2017-01-01 13:33:45 -0500

    grayfx now parses style attribute for color

--- Tools/grayfx/src/emitter.rs
+++ Tools/grayfx/src/emitter.rs
@@ -3,17 +3,57 @@ extern crate xml;
 use polyfill;
 
 use std::str::FromStr;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 /// Parse a float with appropriate panic message on failure.
 macro_rules! parse_float {
     ($s:expr) => (f64::from_str($s).expect("Failed to parse float"))
 }
 
+pub struct Color {
+    r: u8, g: u8, b: u8
+}
+
+impl Color {
+    pub fn black() -> Color { Color { r: 0, g: 0, b: 0 } }
+    // pub fn white() -> Color { Color { r: u8::max_value(), g: u8::max_value(), b: u8::max_value() } }
+
+    pub fn parse(hexcode: &str) -> Color {
+        let mut chars = hexcode.chars();
+
+        // Skip over the leading '#'
+        {
+            let hash = chars.next().expect("Empty string passed as color hexcode");
+            assert_eq!(hash, '#');
+        }
+
+        // Grab 2 chars, parse those as hexadecimal
+        let mut next_component = || {
+            let a = chars.next().expect("Ran out of chars while parsing color hexcode");
+            let b = chars.next().expect("Ran out of chars while parsing color hexcode");
+            let ab = String::from_utf8(vec![a as u8, b as u8]).expect("What");
+            u8::from_str_radix(&ab, 16).expect("Invalid hex number in color string")
+        };
+
+        let r = next_component();
+        let g = next_component();
+        let b = next_component();
+
+        Color { r:r, g:g, b:b }
+    }
+
+    pub fn emit(&self) -> String {
+        format!(
+            "cocos2d::Color4F(cocos2d::Color3B({},{},{}))",
+            self.r, self.g, self.b
+        )
+    }
+}
+
 
 trait Shape {
     /// Should generate code to draw the shape on a cocos2dx DrawNode
-    fn emit_graphics(&self, id: &str, drawnode: &str, color: &str);
+    fn emit_graphics(&self, id: &str, drawnode: &str, color: Option<&str>);
     /// Should generate code for edge segments, and an encompassing shape
     ///
     /// TODO probably also make those generated shapes have certain categories
@@ -21,13 +61,15 @@ trait Shape {
     fn emit_physics(&self, id: &str, physicsbody: &str);
 }
 
+
 pub struct Polygon {
     verts:     Vec<[f64; 2]>,
-    triangles: Vec<[usize; 3]>
+    triangles: Vec<[usize; 3]>,
+    color:     Color
 }
 
 impl Shape for Polygon {
-    fn emit_graphics(&self, id: &str, drawnode: &str, color: &str) {
+    fn emit_graphics(&self, id: &str, drawnode: &str, color: Option<&str>) {
         println!("// Triangles for {}", id);
         for ref t in &self.triangles {
             println!(
@@ -36,7 +78,7 @@ impl Shape for Polygon {
                 self.verts[t[0]][0], self.verts[t[0]][1],
                 self.verts[t[1]][0], self.verts[t[1]][1],
                 self.verts[t[2]][0], self.verts[t[2]][1],
-                color
+                color.unwrap_or(&self.color.emit())
             );
         }
     }
@@ -74,21 +116,23 @@ impl Shape for Polygon {
     }
 }
 
+
 pub struct Circle {
     cx: f64,
     cy: f64,
-    r:  f64
+    r:  f64,
+    color: Color
 }
 
 impl Shape for Circle {
-    fn emit_graphics(&self, id: &str, drawnode: &str, color: &str) {
+    fn emit_graphics(&self, id: &str, drawnode: &str, color: Option<&str>) {
         println!("// Circle for {}", id);
         // arguments: center, radius, angle, segments, color
         println!(
             "{}->drawSolidCircle(Vec2({:.10}f,{:.10}f), {:.10}f, 0.0f, 20, {});",
             Emitter::varname(id, drawnode),
             self.cx, self.cy, self.r,
-            color
+            color.unwrap_or(&self.color.emit())
         );
     }
 
@@ -101,11 +145,12 @@ pub struct Rect {
     x: f64,
     y: f64,
     w: f64,
-    h: f64
+    h: f64,
+    color: Color
 }
 
 impl Shape for Rect {
-    fn emit_graphics(&self, id: &str, drawnode: &str, color: &str) {
+    fn emit_graphics(&self, id: &str, drawnode: &str, color: Option<&str>) {
         println!("// Rect for {}", id);
         // arguments: origin, destination, color
         println!(
@@ -113,7 +158,7 @@ impl Shape for Rect {
             Emitter::varname(id, drawnode),
             self.x,        self.y,
             self.x+self.w, self.y+self.h,
-            color
+            color.unwrap_or(&self.color.emit())
         );
     }
 
@@ -142,14 +187,14 @@ impl Shape for Rect {
 /// The emitter itself. Holds onto shapes relative to their id.
 pub struct Emitter {
     origin: Option<[f64; 2]>,
-    shapes: HashMap<String, Box<Shape>>
+    shapes: BTreeMap<String, Box<Shape>>
 }
 
 impl Emitter {
     pub fn new() -> Emitter {
         Emitter {
             origin: None,
-            shapes: HashMap::new()
+            shapes: BTreeMap::new()
         }
     }
 
@@ -162,7 +207,7 @@ impl Emitter {
         match self.shapes.get(id) {
             Some(shape) =>{
                 if let Some(dn) = drawnode {
-                    shape.emit_graphics(id, dn, &color.unwrap_or("Color4F::WHITE"));
+                    shape.emit_graphics(id, dn, color);
                 }
                 if let Some(pb) = physicsbody {
                     shape.emit_physics(id, pb);
@@ -179,7 +224,7 @@ impl Emitter {
     pub fn emit_all(&self, drawnode: Option<&str>, physicsbody: Option<&str>, color: Option<&str>) {
         for (ref id, ref shape) in &self.shapes {
             if let Some(dn) = drawnode {
-                shape.emit_graphics(id, dn, &color.unwrap_or("Color4F::WHITE"));
+                shape.emit_graphics(id, dn, color);
             }
             if let Some(pb) = physicsbody {
                 shape.emit_physics(id, pb);
@@ -254,9 +299,14 @@ impl Emitter {
             }
         }
 
+        let ref style = attributes.iter().find(|ref a| a.name.local_name == "style")
+            .expect("Path lacking 'style' attribute")
+            .value;
+
         Polygon {
             triangles: polyfill::triangle_indices(&verts, polyfill::TANGENTIAL),
-            verts: verts
+            verts: verts,
+            color: Emitter::parse_color_from_style(style).unwrap_or(Color::black())
         }
     }
 
@@ -264,8 +314,8 @@ impl Emitter {
     /// Parse a circle with center point based on `self.origin`.
     ///
     pub fn parse_circle(&mut self, attributes: &Vec<xml::attribute::OwnedAttribute>) -> Circle {
-        //                 cx    cy    r
-        let mut params = (None, None, None);
+        //                 cx    cy    r    color
+        let mut params = (None, None, None, None);
 
         for ref attr in attributes {
             let name: &str = &attr.name.local_name;
@@ -273,13 +323,17 @@ impl Emitter {
                 "cx" => params.0 = Some(attr.value.clone()),
                 "cy" => params.1 = Some(attr.value.clone()),
                 "r"  => params.2 = Some(attr.value.clone()),
+                "style"  => params.3 = Some(attr.value.clone()),
                 _ => {}
             }
 
-            if let (Some(cx), Some(cy), Some(r)) = params {
+            if let (Some(cx), Some(cy), Some(r), Some(style)) = params {
                 let mut cxy = [0.0, 0.0];
                 self.assign_position_from_origin(&mut cxy, &cx, &cy);
-                return Circle { cx: cxy[0], cy: cxy[1], r: parse_float!(&r) };
+                return Circle {
+                    cx: cxy[0], cy: cxy[1], r: parse_float!(&r),
+                    color: Emitter::parse_color_from_style(&style).unwrap_or(Color::black())
+                };
             }
         }
         panic!("Invalid circle");
@@ -289,8 +343,8 @@ impl Emitter {
     /// Parse a rect with origin at the bottom right (??)
     ///
     pub fn parse_rect(&mut self, attributes: &Vec<xml::attribute::OwnedAttribute>) -> Rect {
-        //                 x,    y,    w,    h
-        let mut params = (None, None, None, None);
+        //                 x,    y,    w,    h,   style
+        let mut params = (None, None, None, None, None);
 
         for ref attr in attributes {
             let name: &str = &attr.name.local_name;
@@ -299,17 +353,19 @@ impl Emitter {
                 "y"      => params.1 = Some(attr.value.clone()),
                 "width"  => params.2 = Some(attr.value.clone()),
                 "height" => params.3 = Some(attr.value.clone()),
+                "style"  => params.4 = Some(attr.value.clone()),
                 _ => {}
             }
 
-            if let (Some(x), Some(y), Some(w), Some(h)) = params {
+            if let (Some(x), Some(y), Some(w), Some(h), Some(style)) = params {
                 let mut xy = [0.0, 0.0];
                 self.assign_position_from_origin(&mut xy, &x, &y);
                 let fw = parse_float!(&w);
                 let fh = parse_float!(&h);
                 return Rect {
                     x: xy[0], y: xy[1] - fh,
-                    w: fw,    h: fh
+                    w: fw,    h: fh,
+                    color: Emitter::parse_color_from_style(&style).unwrap_or(Color::black())
                 }
             }
         }
@@ -317,6 +373,20 @@ impl Emitter {
     }
 
     // =========== Private helper functions ===========
+
+    fn parse_color_from_style(style: &str) -> Option<Color> {
+        let exprs = style.split(';');
+        for ref expr in exprs {
+            let mut keyval = expr.split(':');
+            let key = keyval.next().expect("Invalid style entry");
+
+            if key == "fill" {
+                let val = keyval.next().expect("Invalid style entry");
+                return Some(Color::parse(val));
+            }
+        }
+        None
+    }
 
     fn varname(id: &str, drawnode: &str) -> String {
         if drawnode.contains("{id}") {
--- generate-code.ps1
+++ generate-code.ps1
@@ -3,5 +3,5 @@ Set-Alias grayfx .\Tools\grayfx\target\debug\grayfx.exe
 grayfx .\Resource-Dev\character-wip.svg  -i body,head  -c color  -d _idleFrame `
        | Out-File .\Classes\Generated\CharIdle.h -Encoding utf8
 
-grayfx .\Resource-Dev\test-level.svg  -d ground  -p "ground->getPhysicsBody()"  -c "Color4F::BLACK" `
+grayfx .\Resource-Dev\test-level.svg  -d ground  -p "ground->getPhysicsBody()" `
        | Out-File .\Classes\Generated\TestLevel.h -Encoding utf8
